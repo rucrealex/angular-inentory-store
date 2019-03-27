8a7554eccbabeb51491f83ba775ba7daa89d71b5
Alexey Popov <alexey.popov@rp.ru>
2019-03-27 16:21:20 +0300

    develop inventory application bu ng-book2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductRowComponent } from './product-row/product-row.component';
+import { ProductDepartmentComponent } from './product-department/product-department.component';
+import { ProductImageComponent } from './product-image/product-image.component';
+import { PriceDisplayComponent } from './price-display/price-display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductRowComponent,
+    ProductDepartmentComponent,
+    ProductImageComponent,
+    PriceDisplayComponent
   ],
   imports: [
     BrowserModule,
